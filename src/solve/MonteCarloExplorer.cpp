fd4c634d7b17c24f2698ae3d0c3b643a8373fdd5
Compte etudiant <etudiant@8FL.localdomain>
2014-10-23 14:02:01 +0200

    metropolis

--- src/solve/MonteCarloExplorer.cpp
+++ src/solve/MonteCarloExplorer.cpp
@@ -1,19 +1,63 @@
 #include "MonteCarloExplorer.h"
 
 using namespace solve;
+using namespace std;
 
 // Constructors/Destructors
 //  
 
+
 MonteCarloExplorer::MonteCarloExplorer ( ) {
+	temperature = 4.5;
+	frequence = 1000;
+	// problem is set with SIBus adapter
 }
 
 MonteCarloExplorer::~MonteCarloExplorer ( ) { }
+
+////////////////////////////////
+///// getters & setters ////////
+////////////////////////////////
+
+double MonteCarloExplorer::getTemperature() {
+	return temperature;
+}
+
+int MonteCarloExplorer::getFrequence() {
+	return frequence;
+}
+
+
 
 //  
 // Methods
 //  
 
+void MonteCarloExplorer::heuristic() {
+	srand(time(NULL));
+
+	metropolis(5);
+}
+
+
+// true => acceptance else false
+// delta will be the current state - the new state => if the new one has more conflicts then it becomes the current state
+bool MonteCarloExplorer::metropolis(int delta){
+	double value = exp(delta / temperature);
+	double randValue = rand() % 10;
+	if (delta < 0 || value > randValue) {
+		decreaseTemperature();
+		return true;
+	}
+	else {
+		decreaseTemperature();
+		return false;
+	}
+}
+
+void MonteCarloExplorer::decreaseTemperature() {
+	temperature = temperature * 0.98;
+}
 
 // Accessor methods
 //  
